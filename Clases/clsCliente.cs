cd7be5588f0c47415ce11cbc7491678888693fde
Bymarium <bymarium@gmail.com>
2025-05-28 17:42:09 -0500

    crear servicio para registrar cliente

---
+++ Clases/clsCliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VentaAutos.Models;
+
+namespace VentaAutos.Clases
+{
+  public class clsCliente
+  {
+    private db20311Entities dbVenta = new db20311Entities();
+    public Cliente cliente { get; set; }
+
+    public String Registrar()
+    {
+      try
+      {
+        dbVenta.Cliente.Add(cliente);
+        dbVenta.SaveChanges();
+        return "Se ha registrado exitosamente el cliente";
+      }
+      catch (Exception ex)
+      {
+        return "No se ha podido registrar el cliente" + ex.Message;
+      }
+    }
+  }
+}
