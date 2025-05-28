5023bd8bf7684083749103cd559862a40b857784
MANUELAG\manue <manuelagomezz1234chaparro@gmail.com>
2025-05-28 12:02:08 -0500

    Servicio de reclamo garantia

---
+++ Clases/clsReclamoGarantia.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VentaAutos.Models;
+
+namespace VentaAutos.Clases
+{
+    public class clsReclamoGarantia
+    {
+        private db20311Entities dbVenta = new db20311Entities();
+        public ReclamoGarantia reclamoGarantia { get; set; }
+
+        public List<ReclamoGarantia> ConsultarTodos()
+        {
+            return dbVenta.ReclamoGarantia.ToList();
+        }
+
+        public String Insertar()
+        {
+            try
+            {
+                dbVenta.ReclamoGarantia.Add(reclamoGarantia);
+                dbVenta.SaveChanges();
+                return "Se ha ingresado con éxito un reclamo de la garantía";
+            }
+            catch (Exception ex)
+            {
+                return "No se ha podido ingresar el reclamo de la garantía" + ex.Message;
+            }
+
+
+        }
+
+       
+    }
+}
---
+++ Controllers/ReclamoGarantiaController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VentaAutos.Clases;
+using VentaAutos.Models;
+
+namespace VentaAutos.Controllers
+{
+    [RoutePrefix("Reclamo/Garantia")]
+    public class ReclamoGarantiaController : ApiController
+    {
+        [HttpGet]
+        [Route("ConsultarTodos")]
+        public List<ReclamoGarantia> ConsultarTodos()
+        {
+            clsReclamoGarantia clsreclamoGarantia = new clsReclamoGarantia();
+            return clsreclamoGarantia.ConsultarTodos();
+        }
+
+
+        [HttpPost]
+        [Route("Insertar")]
+        public string Insertar([FromBody] ReclamoGarantia reclamoGarantia)
+        {
+            clsReclamoGarantia clsreclamoGarantia = new clsReclamoGarantia();
+            clsreclamoGarantia.reclamoGarantia = reclamoGarantia;
+            return clsreclamoGarantia.Insertar();
+        }
+
+    }
+}
