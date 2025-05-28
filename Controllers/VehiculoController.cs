30da6b06aa36432dc2cf2ad8ec83a2e2dd4d73c0
Bymarium <bymarium@gmail.com>
2025-05-28 17:52:05 -0500

    crear controlador y metodo get para listar vehiculos disponibles

---
+++ Controllers/VehiculoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VentaAutos.Clases;
+using VentaAutos.Models;
+
+namespace VentaAutos.Controllers
+{
+  [RoutePrefix("api/Vehiculo")]
+  public class VehiculoController : ApiController
+  {
+    [HttpGet]
+    [Route("ListarDisponibles")]
+    public List<Vehiculo> ListarDisponibles()
+    {
+      clsVehiculo vehiculos = new clsVehiculo();
+      return vehiculos.ListarDisponibles();
+    }
+  }
+}
