e556ec389a12f846e46dccb814e4d3990ca2a3a9
Bymarium <bymarium@gmail.com>
2025-05-28 18:59:23 -0500

    crear metodo para generar factura de un pedido aprobado

---
+++ Clases/clsFactura.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VentaAutos.Models;
+
+namespace VentaAutos.Clases
+{
+  public class clsFactura
+  {
+    private db20311Entities dbVenta = new db20311Entities();
+
+    public FacturaVenta GenerarFacturaDesdePedido(int numeroPedido, string documentoEmpleado)
+    {
+      var pedido = dbVenta.PedidoCliente.FirstOrDefault(p => p.Id == numeroPedido && p.Estado == "Aprobado");
+
+      if (pedido == null)
+      {
+        throw new Exception("El pedido no existe o no está aprobado.");
+      }
+
+      var empleadoCargo = dbVenta.EmpleadoCargo
+          .FirstOrDefault(e => e.DocumentoEmpleado == documentoEmpleado && e.FechaFin == null);
+
+      if (empleadoCargo == null)
+      {
+        throw new Exception("No se encontró el empleado activo.");
+      }
+
+      FacturaVenta factura = new FacturaVenta
+      {
+        DocumentoCliente = pedido.DocumentoCliente,
+        CodigoEmpleadoCargo = empleadoCargo.Codigo,
+        Fecha = DateTime.Now,
+        DetalleFacturaVenta = new List<DetalleFacturaVenta>()
+      };
+
+      foreach (var detallePedido in pedido.DetallePedidoCliente)
+      {
+        var vehiculo = dbVenta.Vehiculo.Find(detallePedido.CodigoVehiculo);
+
+        if (vehiculo != null)
+        {
+          var detalleFactura = new DetalleFacturaVenta
+          {
+            CodigoVehiculo = vehiculo.Codigo,
+            Cantidad = detallePedido.Cantidad,
+            ValorUnitario = vehiculo.ValorUnitario
+          };
+          factura.DetalleFacturaVenta.Add(detalleFactura);
+        }
+      }
+
+      dbVenta.FacturaVenta.Add(factura);
+      dbVenta.SaveChanges();
+
+      return factura;
+    }
+  }
+
+}
