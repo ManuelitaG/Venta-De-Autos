ea0d3c49c32731f261c550d0f68c4ee73bd2ba6f
MANUELAG\manue <manuelagomezz1234chaparro@gmail.com>
2025-05-28 12:46:43 -0500

    Servicios de nota credito

---
+++ Clases/clsNotaCredito.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VentaAutos.Models;
+
+namespace VentaAutos.Clases
+{
+    public class clsNotaCredito
+    {
+        private db20311Entities dbVenta = new db20311Entities();
+        public NotaCredito notaCredito { get; set; }
+
+        public List<NotaCredito> ConsultarTodos()
+        {
+            return dbVenta.NotaCredito.ToList();
+        }
+
+        public String Insertar()
+        {
+            try
+            {
+                dbVenta.NotaCredito.Add(notaCredito);
+                dbVenta.SaveChanges();
+                return "Se ha ingresado con éxito una nota credito";
+            }
+            catch (Exception ex)
+            {
+                return "No se ha podido ingresar una nota credito" + ex.Message;
+            }
+
+
+        }
+    }
+}
