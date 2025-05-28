4a13ae55191b124d4f6ebd9edbb0a872895f5b90
Bymarium <bymarium@gmail.com>
2025-05-28 18:00:35 -0500

    crear servicio para crear y consultar marca

---
+++ Clases/clsMarca.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VentaAutos.Models;
+
+namespace VentaAutos.Clases
+{
+  public class clsMarca
+  {
+    private db20311Entities dbVenta = new db20311Entities();
+    public Marca marca { get; set; }
+
+    public List<Marca> ConsultarTodos()
+    {
+      return dbVenta.Marca.ToList();
+    }
+
+    public String Insertar()
+    {
+      try
+      {
+        dbVenta.Marca.Add(marca);
+        dbVenta.SaveChanges();
+        return "Se ha registrado exitosamente la marca";
+      }
+      catch (Exception ex)
+      {
+        return "No se ha podido registrar la marca" + ex.Message;
+      }
+    }
+  }
+}
