b700b8337a3a3c703a51e9bbc47dae34f507d418
Bymarium <bymarium@gmail.com>
2025-05-28 17:46:03 -0500

    crear controlador y metodo post para registrar cliente

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VentaAutos.Clases;
+using VentaAutos.Models;
+
+namespace VentaAutos.Controllers
+{
+  [RoutePrefix("api/Cliente")]
+  public class ClienteController : ApiController
+  {
+    [HttpPost]
+    [Route("Registrar")]
+    public string Registrar([FromBody] Cliente cliente)
+    {
+      clsCliente clientes = new clsCliente();
+      clientes.cliente = cliente;
+
+      return clientes.Registrar();
+    }
+  }
+}
