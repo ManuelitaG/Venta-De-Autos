fc5fac623376df05516c026e58dd8a9aebf12ed3
Bymarium <bymarium@gmail.com>
2025-05-28 18:06:43 -0500

    crear servicio para crear y consultar modelo

---
+++ Clases/clsModelo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VentaAutos.Models;
+
+namespace VentaAutos.Clases
+{
+  public class clsModelo
+  {
+    private db20311Entities dbVenta = new db20311Entities();
+    public Modelo modelo { get; set; }
+
+    public List<Modelo> ConsultarTodos()
+    {
+      return dbVenta.Modelo.ToList();
+    }
+
+    public String Insertar()
+    {
+      try
+      {
+        dbVenta.Modelo.Add(modelo);
+        dbVenta.SaveChanges();
+        return "Se ha registrado exitosamente el modelo";
+      }
+      catch (Exception ex)
+      {
+        return "No se ha podido registrar el modelo" + ex.Message;
+      }
+    }
+  }
+}
