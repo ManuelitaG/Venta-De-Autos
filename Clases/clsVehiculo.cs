3c4210887558279fb9175a6572ba9dcca9d5c907
Bymarium <bymarium@gmail.com>
2025-05-28 17:50:11 -0500

    crear servicio para listar vehiculos disponibles

---
+++ Clases/clsVehiculo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VentaAutos.Models;
+
+namespace VentaAutos.Clases
+{
+  public class clsVehiculo
+  {
+    private db20311Entities dbVenta = new db20311Entities();
+    public Vehiculo vehiculo { get; set; }
+
+    public List<Vehiculo> ListarDisponibles()
+    {
+      return dbVenta.Vehiculo
+                 .Where(v => v.Estado == "Disponible")
+                 .ToList();
+    }
+  }
+}
