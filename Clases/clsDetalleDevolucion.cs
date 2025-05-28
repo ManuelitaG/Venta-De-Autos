03acae1c638ed5d9279ebce8fa0795efe8ce4d28
MANUELAG\manue <manuelagomezz1234chaparro@gmail.com>
2025-05-28 12:38:58 -0500

    Detalle devolucion

---
+++ Clases/clsDetalleDevolucion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VentaAutos.Models;
+
+namespace VentaAutos.Clases
+{
+    public class clsDetalleDevolucion
+    {
+        private db20311Entities dbVenta = new db20311Entities();
+        public DetalleDevolucion detalleDevolucion { get; set; }
+
+        public List<DetalleDevolucion> ConsultarTodos()
+        {
+            return dbVenta.DetalleDevolucion.ToList();
+        }
+
+        public String Insertar()
+        {
+            try
+            {
+                dbVenta.DetalleDevolucion.Add(detalleDevolucion);
+                dbVenta.SaveChanges();
+                return "Se ha ingresado con éxito el detalle de una devolucion";
+            }
+            catch (Exception ex)
+            {
+                return "No se ha podido ingresar el detalle de la devolucion" + ex.Message;
+            }
+
+
+        }
+    }
+}
---
+++ Controllers/DetalleDevolucionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VentaAutos.Clases;
+using VentaAutos.Models;
+
+namespace VentaAutos.Controllers
+{
+    [RoutePrefix("api/DetalleDevolucion")]
+    public class DetalleDevolucionController : ApiController
+    {
+        [HttpGet]
+        [Route("ConsultarTodos")]
+        public List<DetalleDevolucion> ConsultarTodos()
+        {
+            clsDetalleDevolucion clsDetalleDevolucion = new clsDetalleDevolucion();
+            return clsDetalleDevolucion.ConsultarTodos();
+        }
+
+
+        [HttpPost]
+        [Route("Insertar")]
+        public string Insertar([FromBody] DetalleDevolucion detalleDevolucion)
+        {
+            clsDetalleDevolucion clsdetalleDevolucion = new clsDetalleDevolucion();
+            clsdetalleDevolucion.detalleDevolucion = detalleDevolucion;
+            return clsdetalleDevolucion.Insertar();
+        }
+
+    }
+}
