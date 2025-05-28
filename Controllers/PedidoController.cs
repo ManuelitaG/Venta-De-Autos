bec58e4246f25a33558a4ada85ce6c5c0aa7e153
Bymarium <bymarium@gmail.com>
2025-05-28 18:41:29 -0500

    crear controlador y metodo post para crear pedido

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VentaAutos.Clases;
+using VentaAutos.Models;
+
+namespace VentaAutos.Controllers
+{
+  [RoutePrefix("api/Pedido")]
+  public class PedidoController : ApiController
+  {
+    [HttpPost]
+    [Route("Insertar")]
+    public string Registrar(PedidoCliente pedido, List<DetallePedidoCliente> detalles)
+    {
+      clsPedido pedidos = new clsPedido();
+      pedidos.pedido = pedido;
+      pedidos.detalles = detalles;
+
+      return pedidos.Insertar();
+    }
+  }
+}
