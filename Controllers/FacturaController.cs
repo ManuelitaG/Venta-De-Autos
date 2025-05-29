4ae9de993572d5f01999d97ce80cc6e83be8be47
Bymarium <bymarium@gmail.com>
2025-05-28 19:02:16 -0500

    crear metodo post para generar factura de un pedido aprobado

---
+++ Controllers/FacturaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VentaAutos.Clases;
+using VentaAutos.Models;
+
+namespace VentaAutos.Controllers
+{
+  [RoutePrefix("api/Factura")]
+  public class FacturaController : ApiController
+  {
+    [HttpPost]
+    [Route("GenerarFactura")]
+    public FacturaVenta GenerarFactura(int numeroPedido, string documentoEmpleado)
+    {
+      clsFactura facturas = new clsFactura();
+
+      return facturas.GenerarFacturaDesdePedido(numeroPedido, documentoEmpleado);
+    }
+  }
+}
