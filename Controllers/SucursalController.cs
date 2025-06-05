d766caa112c2d8494de8dfaf04b735ead0c88885
Bymarium <bymarium@gmail.com>
2025-06-05 09:41:54 -0500

    crear controlador con metodos get y post para sucursal

---
+++ Controllers/SucursalController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VentaAutos.Clases;
+using VentaAutos.Models;
+
+namespace VentaAutos.Controllers
+{
+  [RoutePrefix("api/Sucursal")]
+  public class SucursalController : ApiController
+  {
+    [HttpGet]
+    [Route("ConsultarTodos")]
+    public List<Sucursal> ConsultarTodos()
+    {
+      clsSucursal sucursales = new clsSucursal();
+      return sucursales.ConsultarTodos();
+    }
+
+    [HttpPost]
+    [Route("Registrar")]
+    public string Registrar([FromBody] Sucursal sucursal)
+    {
+      clsSucursal sucursales = new clsSucursal();
+      sucursales.sucursal = sucursal;
+
+      return sucursales.Insertar();
+    }
+  }
+}
