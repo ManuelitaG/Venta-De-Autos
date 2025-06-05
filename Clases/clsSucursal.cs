c0261f8f636228248200fa10e3cc8d3d40e261d6
Bymarium <bymarium@gmail.com>
2025-06-05 09:38:11 -0500

    crear servicio para agregar y listar sucursales

---
+++ Clases/clsSucursal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VentaAutos.Models;
+
+namespace VentaAutos.Clases
+{
+  public class clsSucursal
+  {
+    private db20311Entities dbVenta = new db20311Entities();
+    public Sucursal sucursal { get; set; }
+
+    public List<Sucursal> ConsultarTodos()
+    {
+      return dbVenta.Sucursal.ToList();
+    }
+
+    public String Insertar()
+    {
+      try
+      {
+        dbVenta.Sucursal.Add(sucursal);
+        dbVenta.SaveChanges();
+        return "Se ha ingresado con éxito una sucursal";
+      }
+      catch (Exception ex)
+      {
+        return "No se ha podido ingresar la sucursal" + ex.Message;
+      }
+    }
+  }
+}
